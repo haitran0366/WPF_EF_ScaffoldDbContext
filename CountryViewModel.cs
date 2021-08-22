b30b4be553542b1a9ece5e49dc2c4f2646665005
haitran0366 <haitran_usa@yahoo.com>
2021-08-22 12:17:25 -0700

    Add project files.

---
+++ CountryViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF_EF_ScaffoldDbContext.Models;
+
+namespace WPF_EF_ScaffoldDbContext
+{
+    public class CountryViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string name ="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private string countryName;
+        public string CountryName
+        {
+            get { return countryName; }
+            set
+            {
+                countryName = value;
+                NotifyPropertyChanged("CountryName");
+            }
+        }
+
+        private string greeting;
+        public string Greeting
+        {
+            get { return greeting; }
+            set
+            {
+                greeting = value;
+                NotifyPropertyChanged("Greeting");
+            }
+        }
+        private string errorMessage;
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set
+            {
+                errorMessage = value;
+                NotifyPropertyChanged("ErrorMessage");
+            }
+        }
+
+        public ICommand cmdSave { get; private set; }
+        public bool CanExectute
+        {
+            get { return !string.IsNullOrEmpty(CountryName) & !string.IsNullOrEmpty(Greeting); }
+        }
+
+
+
+        public CountryViewModel()
+        {
+            cmdSave = new RelayCommand(Save, () => CanExectute);
+        }
+
+        private void Save()
+        {
+            try
+            {
+                using (var context = new WPF_DBContext())
+                {
+                    var country = new TblCountry()
+                    {
+                        CountryName = CountryName,
+                        Greeting = Greeting
+                    };
+                    context.Add(country);
+                    context.SaveChanges();
+                }
+            }
+            catch(Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+            ErrorMessage = "Data saved successfully!";
+        }
+    }
+}
---
+++ Models/TblCountry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WPF_EF_ScaffoldDbContext.Models
+{
+    public partial class TblCountry
+    {
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+        public string Greeting { get; set; }
+    }
+}
